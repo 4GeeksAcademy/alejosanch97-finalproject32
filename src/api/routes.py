a0bbdeece4f2ff3fd6fbf84d69f57f0d062c54fb
José Hernández <josemiguelhernandezsousa@gmail.com>
2024-08-22 02:52:37 +0000

    add task route

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Tasks
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -21,3 +21,24 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+
+@api.route('/add-task', methods=['POST'])
+def add_task():
+    data = request.json
+    task_name = data.get("name")
+    task_description = data.get("description")
+    task_due_date = data.get("due_date")
+    task = Tasks(
+        name= task_name,
+        description= task_description,
+        due_date = task_due_date
+    )
+
+    try:
+        db.session.add(task)
+        db.session.commit()
+        return jsonify({"message":"task created"})
+    except Exception as error:
+        print(error.args)
+        db.session.rollback()
+        return jsonify({"message":error})
