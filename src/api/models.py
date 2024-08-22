b20058989b979d5258cb30174317008cbc243e35
MaverickOchoa <126739291+MaverickOchoa@users.noreply.github.com>
2024-08-22 02:34:38 +0000

    creando routes

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(100), unique=False, nullable=True)
+    last_name = db.Column(db.String(150), unique=False, nullable=True)
+    
+    update_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
     def __repr__(self):
         return f'<User {self.email}>'
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,15 +2,23 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
+from flask_migrate import Migrate
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from admin import setup_admin
 from flask_cors import CORS
 
+import requests
+
 api = Blueprint('api', __name__)
+api.url_map.strict_slashes = False
+
 
 # Allow CORS requests to this API
+MIGRATE = Migrate(api, db)
+db.init_api(api)
 CORS(api)
-
+setup_admin(api)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -20,3 +28,50 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/user', methods=["POST"])
+def add_user():
+    body = request.json
+
+    first_name = body.get("name", None)
+    last_name = body.get("last_name", None)
+    username = body.get("username", None)
+    email = body.get("email", None)
+    password = body.get("password", None)
+    role_id = body.get("role_id", None)
+
+#get all users
+@api.route('/users', methods=["GET"])
+def get_all_users():
+    users = User()
+    users = users.query.all()
+    users = list(map(lambda item: item.serialize(), users))
+
+    return jsonify(users , 200)
+
+# get users by id
+@api.route('/user/<int:user_id>', methods=["GET"])
+def get_user(user_id):
+    user = User()
+    user = user.query.get(user_id)
+
+    if user is None:
+        raise APIException("User not found", status_code=404)
+    else: 
+        return jsonify(user.serialize())
+    
+#add projects
+@api.route('/project', methods=["POST"])
+def ad_project():
+    body = request.json
+
+    name =  body.get("name", None)
+    description = body.get("description", None)
+    start_date = body.get("start_date", None)
+    end_date = body.get("end_date", None)
+
+# get all projects
+@api.route('/puejects', methods=["GET"])
+def get_all_projects():
+    projects = Projects()
+    projects = projects.query.all()
